593d02cb50c4fcc3f4307e6c442e467169812101
Lacty <akira206@gmail.com>
2016-02-01 19:14:56 +0900

    operation check for glfw3

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,29 @@
 
 #include <iostream>
+#include <GLFW/glfw3.h>
+
 #include "header/debugIO.hpp"
 
 
 int main() {
-#if DEBUG_IO
-  std::cout << "debug mode" << std::endl;
-#else
-  std::cout << "release mode" << std::endl;
-#endif
+  if (!glfwInit()) return -1;
+
+  GLFWwindow* window = glfwCreateWindow(640, 480, "HELLO", nullptr, nullptr);
+  if (!window) {
+    glfwTerminate();
+    return -1;
+  }
+
+  glfwMakeContextCurrent(window);
+
+  while (!glfwWindowShouldClose(window)) {
+    glClearColor(0.4f, 0.4f, 0.4f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
+  return 0;
 }
