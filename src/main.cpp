0eb6aaab974e9038a77a35cf78e916527ba1e6ec
Lacty <akira206@gmail.com>
2016-02-10 00:23:20 +0900

    add

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,27 @@
 
 #include <iostream>
+#include <GLFW/glfw3.h>
 
 
 int main() {
-  int a = 0;
-  std::cout << "a = " << a << std::endl;
+  if(!glfwInit()) return 0;
+
+  auto window = glfwCreateWindow(640, 480, "GetSpeed2", nullptr, nullptr);
+  if(!window) {
+    glfwTerminate();
+    return 0;
+  }
+
+  glfwMakeContextCurrent(window);
+
+  while(!glfwWindowShouldClose(window)) {
+    glClearColor(0.4f, 0.4f, 0.4f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
+  return 0;
 }
