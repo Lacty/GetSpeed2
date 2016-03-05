ad4b4db48baa692751405ac32111ea29fbdede6d
Lacty <akira206@gmail.com>
2016-03-05 18:21:08 +0900

    impl toRadians

--- src/Utility/utility.cpp
+++ src/Utility/utility.cpp
@@ -1,2 +1,7 @@
 
 #include "utility.hpp"
+
+
+float toRadians(float deg) {
+  return float(deg * M_PI) / 180.0f;
+}
--- src/Utility/utility.hpp
+++ src/Utility/utility.hpp
@@ -1,2 +1,8 @@
 
 #pragma once
+#include <cmath>
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+
+float toRadians(float deg);
