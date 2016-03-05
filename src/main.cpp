958dc02201c2a921c0b14e90436725c3ee3d3954
Lacty <akira206@gmail.com>
2016-03-05 18:14:34 +0900

    add Utility

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,11 @@
 
 #include "AppNative/appNative.hpp"
 
+// Todo: Vector: impl norm/cross/dot
 
 int main() {
   AppNative app(Vec2i(600, 600), "Test");
   app.setClearColor(Color::gray());
-
-  float piyo = 0.0f;
-  TwBar* twBar = TwNewBar("Test");
-  TwAddVarRW(twBar, "piyo", TW_TYPE_FLOAT, &piyo, "");
 
   while (app.isOpen()) {
     app.begin();
--- vs2015/src/cmake_install.cmake
+++ vs2015/src/cmake_install.cmake
@@ -43,6 +43,7 @@ if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   include("C:/Users/USER/Desktop/GetSpeed2/vs2015/src/Random/cmake_install.cmake")
   include("C:/Users/USER/Desktop/GetSpeed2/vs2015/src/CatmullRom/cmake_install.cmake")
   include("C:/Users/USER/Desktop/GetSpeed2/vs2015/src/TweakBar/cmake_install.cmake")
+  include("C:/Users/USER/Desktop/GetSpeed2/vs2015/src/Utility/cmake_install.cmake")
 
 endif()
 
