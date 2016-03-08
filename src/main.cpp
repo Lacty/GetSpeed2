7e8b77bc4f4dbf83374cfabfc8e7dac7cf989aba
Lacty <akira206@gmail.com>
2016-03-08 17:18:50 +0900

    フォルダ構成を変更　Freetypeのライブラリを更新

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,18 @@
 
 #include "AppNative/appNative.hpp"
 
+
 int main() {
   AppNative app(Vec2i(600, 600), "Test");
   app.setClearColor(Color::gray());
 
+  Font font(loadAsset("rounded-l-mplus-1c-regular.ttf"));
+  font.setSize(100);
+
   while (app.isOpen()) {
     app.begin();
+
+    font.draw("hoge", Vec2f::zero());
 
     app.end();
   }
